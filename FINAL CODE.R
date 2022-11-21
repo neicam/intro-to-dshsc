ef9227cb5da36c686dc94dfcd6c57be098aec6f7
neicam <116350992+neicam@users.noreply.github.com>
2022-11-21 21:09:18 +0000

    Update FINAL CODE.R

--- FINAL CODE.R
+++ FINAL CODE.R
@@ -27,12 +27,13 @@ Orig_healthutil_raw  %>%
          Value) %>% 
   filter(Measure == "Waiting times from specialist assessment to treatment: Mean (days)",
          Variable == "Hip replacement (total and partial, including the revision of hip replacement)") %>% 
-  ggplot(aes(x = Year, y = Value, group=Country, color = Country)) + 
+  ggplot(aes(x = Year, y = Value, )) + 
   geom_line() +
   geom_point() +
-  labs(title = "Waiting times for Elective Hip Replacement", 
+  facet_wrap(~Country) +
+  labs(title = "Waiting times for Hip Procedures", 
        y = "Mean waiting times from assessment to treatment (days)") +
-  theme(plot.title=element_text(hjust=0.5), axis.text.x = element_text(angle = 0, vjust = 1, hjust = 1), plot.background = element_rect(fill = "lightblue"))
+  theme(plot.title=element_text(hjust=0.5), axis.text.x = element_text(angle = 0, vjust = 1, hjust = 1), plot.background = element_rect(fill = "lightblue")) 
 
 # Wrangling data for doctors and graduates and combining to single dataset
 Orig_medicalgraduates_raw <- Orig_medicalgraduates_raw %>% 
@@ -56,10 +57,10 @@ sPDF <- joinCountryData2Map(mapworld
                             , joinCode = "ISO3"
                             , nameJoinColumn = "ISO3V10")
 mapCountryData(sPDF
-                , nameColumnToPlot="Doctors_and_Graduates_per_1000" 
-                , mapTitle="Doctors and Medical Graduates per 1000 of Population in 2020"
-                , oceanCol='lightblue'
-                , missingCountryCol='white') 
+               , nameColumnToPlot="Doctors_and_Graduates_per_1000" 
+               , mapTitle="Doctors and Medical Graduates per 1000 of Population in 2020"
+               , oceanCol='lightblue'
+               , missingCountryCol='white') 
 
 #Europe Map
 sPDF <- joinCountryData2Map(mapworld
@@ -67,11 +68,11 @@ sPDF <- joinCountryData2Map(mapworld
                             , nameJoinColumn = "ISO3V10")
 
 mapCountryData(sPDF
-                , nameColumnToPlot="Doctors_and_Graduates_per_1000" 
-                , mapTitle="Doctors and Medical Graduates per 1000 of Population in 2020"
-                , oceanCol='lightblue'
-                , missingCountryCol='white'
-                , mapRegion='Europe')
+               , nameColumnToPlot="Doctors_and_Graduates_per_1000" 
+               , mapTitle="Doctors and Medical Graduates per 1000 of Population in 2020"
+               , oceanCol='lightblue'
+               , missingCountryCol='white'
+               , mapRegion='Europe')
 
 #Producing Comparison Table
 HIP_OECD_data <- Orig_healthutil_raw %>% 
@@ -82,20 +83,43 @@ HIP_OECD_data <- Orig_healthutil_raw %>%
 joinedfortablehip <-
   full_join(HIP_OECD_data, doctors_2020_map, by = c("COU" = "LOCATION"))
 
-joinedfortablehip %>% 
-  dplyr::arrange(-Value) %>% 
-  select(Country, 
-         Doctors_and_Graduates_per_1000,
-         Value) %>% 
-  filter(Value != "NA") %>% 
-  filter(Doctors_and_Graduates_per_1000 != "NA") %>% 
+agg_tbl <- joinedfortablehip %>% 
   group_by(Country) %>% 
+  summarise(mean_Value=mean(Value),
+            .groups = 'drop')
+
+agg_tbl2 <- joinedfortablehip %>% 
+  group_by(Country) %>% 
+  summarise(mean_Doctors_and_Graduates_per_1000=mean(Doctors_and_Graduates_per_1000),
+            .groups = 'drop')
+
+joinedfortablehip22 <- 
+  full_join(agg_tbl, agg_tbl2, by = c("Country"))
+
+joinedfortablehip22 %>% 
+  dplyr::arrange(-mean_Value) %>% 
+  select(Country, 
+         mean_Doctors_and_Graduates_per_1000,
+         mean_Value) %>% 
+  filter(mean_Value != "NA") %>% 
+  filter(mean_Doctors_and_Graduates_per_1000 != "NA") %>% 
   select(c(1,2,3)) %>% 
   knitr::kable(caption = "Hip Replacements waiting time against total doctors and medical graduates in 2020", digits = c(0, 1, 0),
                col.names = c("Country",
                              "Doctors & Medical Graduates per 1000",
                              "Waiting times for Hip Procedures Mean days")) %>%
   kable_classic_2()
+
+stat_analysis <- joinedfortablehip22 %>% 
+  dplyr::arrange(-mean_Value) %>% 
+  select(Country, 
+         mean_Doctors_and_Graduates_per_1000,
+         mean_Value) %>% 
+  filter(mean_Value != "NA") %>% 
+  filter(mean_Doctors_and_Graduates_per_1000 != "NA")
+
+cor.test(stat_analysis$mean_Doctors_and_Graduates_per_1000, stat_analysis$mean_Value)
+
 
 
 
